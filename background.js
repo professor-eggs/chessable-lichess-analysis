81f0d3054ee18c3bf69dd3f8fccff802f0652083
Xavier <xavier@lexamica.com>
2023-12-02 20:55:52 -0500

    update manifest to version 3 add screenshot and icon

--- background.js
+++ background.js
@@ -1,11 +1,14 @@
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
   if (
     changeInfo.url &&
     changeInfo.url.includes('https://www.chessable.com/analysis/fen/')
   ) {
-    const fen = changeInfo.url.split('fen/')[1].split('/')[0];
-    const formattedFen = fen.replace(/U/g, '/').replace(/%20/g, ' ');
-    const lichessUrl = `https://lichess.org/analysis/standard/${formattedFen}`;
-    chrome.tabs.update(tabId, { url: lichessUrl });
+    const fenPart = changeInfo.url.split('fen/')[1];
+    if (fenPart) {
+      const fen = fenPart.split('/')[0];
+      const formattedFen = fen.replace(/U/g, '/').replace(/%20/g, ' ');
+      const lichessUrl = `https://lichess.org/analysis/standard/${formattedFen}`;
+      chrome.tabs.update(tabId, { url: lichessUrl });
+    }
   }
 });
